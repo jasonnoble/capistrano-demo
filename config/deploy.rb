c819380cc1e81a8e653527a139b1ada2ddb7b1aa
Jason Noble <perlwizard@gmail.com>
2009-12-05 00:21:00 -0500

    Updated config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,46 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "shopping_cart"
+set :repository, 'git@github.com:jasonnoble/capistrano-demo.git'        
+ 
+# Optional if your git is installed in non-standard 
+# (i.e. not found in path) locations
+# set :scm_command, "/usr/local/git/bin/git" 
+# set :local_scm_command, "git"
+                           
+set :scm, :git                                 
+set :user, "jasonn"
+default_run_options[:pty] = true
+set :branch, "master"
+set :deploy_to, "/var/www/rails/#{user}/#{application}"             
+set :deploy_via, :remote_cache    
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+role :web, "10.130.84.121"                          # Your HTTP server, Apache/etc
+role :app, "10.130.84.121"                          # This may be the same as your `Web` server
+role :db,  "10.130.84.121", :primary => true # This is where Rails migrations will run      
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :use_sudo, false
 
-# namespace :deploy do
-#   task :start {}
-#   task :stop {}
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  namespace :mongrel do
+    [:stop, :start, :restart ].each do |t|
+      desc "#{t.to_s.capitalize} the mongrel appserver"
+      task t, :roles => :app do
+        invoke_command "/opt/ruby-enterprise-1.8.7-2009.10/bin/mongrel_rails cluster::#{t.to_s} -C #{mongrel_conf}", :via => run_method
+      end                                                                                       
+    end
+  end
+       
+  desc "Customer restart task for mongrel cluster"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    deploy.mongrel.restart
+  end
+  desc "Customer start task for mongrel cluster"
+  task :start, :roles => :app, :except => { :no_release => true } do
+    deploy.mongrel.start
+  end
+  desc "Customer stop task for mongrel cluster"
+  task :stop, :roles => :app, :except => { :no_release => true } do
+    deploy.mongrel.stop
+  end
+end
